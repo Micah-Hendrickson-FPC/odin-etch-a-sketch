5ac0414e82f7e829ce15245df839247b7b10234f
Micah-Hendrickson-FPC <micah.hendrickson@fivepackcreative.com>
2025-06-12 14:40:32 -0500

    Fix variable grid sizing issue
    The grid items had hardcoded sizes in styles.css.
    They have been updated so that their sizes are
    assigned dynamically through javascript.
    A future update could be replacing the
    e.target.setAttribute(... part on line 31 of the
    js with something that won't overwrite the prexis-
    ting styles on the grid boxes.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const INITIAL_GRID_SIZE = 16;
-const MAX_GRID_SIZE = 100;
+const MAX_GRID_SIZE = 50;
 
 generateHTMLGrid(INITIAL_GRID_SIZE);
 
 const refreshButton = document.querySelector(".refresh");
 refreshButton.addEventListener("click", () => {
-    let newSize = parseInt(prompt("How many squares should the new grid be?"));
+    const newSize = parseInt(prompt("How many squares should the new grid be?"));
     if (newSize <= MAX_GRID_SIZE && newSize > 0) {
         cleanGrid();
         generateHTMLGrid(newSize);
@@ -15,24 +15,30 @@ refreshButton.addEventListener("click", () => {
 })
 
 function generateHTMLGrid(size) {
-    let gridSize = size;
+    const gridSize = size;
     const container = document.querySelector(".grid-container");
+    const boxSize = Math.floor((1 / size) * 100);
 
     for (let i = 0; i < (gridSize ** 2); i++) {
         const gridBox = document.createElement("div");
         gridBox.classList.add("gridItem");
+        gridBox.setAttribute(`style`, `flex-basis: ${boxSize}%; padding-bottom: ${boxSize}%;`);
         container.appendChild(gridBox);
     }
 
     container.addEventListener("mouseover", (e) => {
         if (e.target != container) {
-            e.target.setAttribute("style", "background-color: grey;");
+            e.target.setAttribute("style", 
+                `background-color: grey;
+                flex-basis: ${boxSize}%;
+                padding-bottom: ${boxSize}%;`
+            );
         }
     });
 }
 
 function cleanGrid() {
-    let container = document.querySelector(".grid-container");
+    const container = document.querySelector(".grid-container");
     while (container.firstChild) {
         container.removeChild(container.lastChild);
     }
