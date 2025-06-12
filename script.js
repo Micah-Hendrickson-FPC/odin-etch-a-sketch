62531469b9e4a67ece5af12ad4154493682b91b3
Micah-Hendrickson-FPC <micah.hendrickson@fivepackcreative.com>
2025-06-12 14:29:36 -0500

    Allow user to create new grid

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 "use strict";
 
-generateHTMLGrid();
+const INITIAL_GRID_SIZE = 16;
+const MAX_GRID_SIZE = 100;
 
-function generateHTMLGrid() {
-    const GRID_SIZE = 16;
+generateHTMLGrid(INITIAL_GRID_SIZE);
+
+const refreshButton = document.querySelector(".refresh");
+refreshButton.addEventListener("click", () => {
+    let newSize = parseInt(prompt("How many squares should the new grid be?"));
+    if (newSize <= MAX_GRID_SIZE && newSize > 0) {
+        cleanGrid();
+        generateHTMLGrid(newSize);
+    }
+})
+
+function generateHTMLGrid(size) {
+    let gridSize = size;
     const container = document.querySelector(".grid-container");
 
-    for (let i = 0; i < (GRID_SIZE ** 2); i++) {
+    for (let i = 0; i < (gridSize ** 2); i++) {
         const gridBox = document.createElement("div");
         gridBox.classList.add("gridItem");
         container.appendChild(gridBox);
@@ -17,4 +29,11 @@ function generateHTMLGrid() {
             e.target.setAttribute("style", "background-color: grey;");
         }
     });
+}
+
+function cleanGrid() {
+    let container = document.querySelector(".grid-container");
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
 }
